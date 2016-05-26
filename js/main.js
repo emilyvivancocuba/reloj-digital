81d4e551d4e5e8aaf564d877086de052488e8a30
emilyvivancocuba <emilyvivancocuba@gmail.com>
2016-05-25 23:24:19 -0500

    hora capturada

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 /*OBTENIENDO*/
 $(document).ready(function(){
-    var captura=new Date();
-   	var horas=captura.getHours();
-   	var minutos=captura.getMinutes();
-   	var segundos=captura.getSeconds();
-   	$("#parrafo_hora").text(horas+':'+minutos+':'+segundos);
+	captura();
+   	
 });
+function captura(){
+	var captura=new Date();
+   	var horas=captura.getHours().toString();
+   	var minutos=captura.getMinutes().toString();
+   	var segundos=captura.getSeconds().toString();
+   	if (parseInt(horas)<10) {
+   		horas='0'+horas;
+   	}
+   	if (parseInt(minutos)<10) {
+   		minutos='0'+minutos;
+   	}
+   	if (parseInt(segundos)<10) {
+   		segundos='0'+segundos;
+   	}
+   	$("#parrafo_hora").text(horas+':'+minutos+':'+segundos);
+}
+
